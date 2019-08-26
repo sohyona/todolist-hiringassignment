2f77bf25046b63e7588fb2046c6a1804329045d0
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-08-26 18:41:16 +0900

    completed add bind link

--- src/Components/TodoListItem.js
+++ src/Components/TodoListItem.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
+// import axios from 'axios';
 
 export default class TodoListItem extends Component {
   constructor (props) {
     super (props);
     this.todoCompleted = {
-      textDecoration: props.todoItem.completed ? 'line-through' : 'none',
+      textDecoration: props.todoItem.completed ? 'line-through' : null,
     };
+    // this.toggleTodoStatus = this.toggleTodoStatus.bind (this);
+  }
+
+  toggleTodoStatus (e) {
+    console.log (e.target.id);
   }
 
   render () {
+    // console.log (this.props.todoItem);
     return (
-      <li className="todoItem" style={this.todoCompleted}>
+      <li
+        className="todoItem"
+        style={this.todoCompleted}
+        id={this.props.todoItem.id}
+        onClick={this.toggleTodoStatus.bind (this)}
+      >
         {this.props.todoItem.title}
       </li>
     );
