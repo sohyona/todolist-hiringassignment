e3b7cfde542ebcc707c7b01b3f42fc7af546923f
Seo Hyona <hyona@Seos-MacBook-Pro.local>
2019-08-26 18:23:24 +0900

    completed display crossed todo item

--- src/Components/TodoListItem.js
+++ src/Components/TodoListItem.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import {NONAME} from 'dns';
 
 export default function TodoListItem (props) {
-  return <li className="todoItem">{props.todoItem.title}</li>;
+  const todoCompleted = {
+    textDecoration: props.todoItem.completed ? 'line-through' : 'none',
+  };
+  return (
+    <li className="todoItem" style={todoCompleted}>{props.todoItem.title}</li>
+  );
 }
